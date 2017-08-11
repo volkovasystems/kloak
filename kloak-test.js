78a8886acd9d8eda6b67ce4e5bad9313879ebf0d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 15:25:56 +0800

    Updated test.

--- kloak-test.js
+++ kloak-test.js
@@ -3,10 +3,10 @@ const assert = require( "assert" );
 const kloak = require( "./kloak.js" );
 
 let hey = function hey( ){ };
-assert.equal( kloak( function hello( ){ }, hey, "yeah" ).name, "hello", "should have name 'hello'" );
+assert.equal( kloak( function hello( ){ }, hey, "yeah" ).name, "hello", "should be equal to 'hello'" );
 
 let f = function f( ){ return "hello" };
 let h = kloak( f, function h( ){ return "world" }, "sample" );
-assert.equal( f( ) == h( ), false, "should return false" );
+assert.equal( f( ) == h( ), false, "should be equal to false" );
 
 console.log( "ok" );
